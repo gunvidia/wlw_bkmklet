c726ab215c2f40a2cc27424eb66ba04240e87ebf
gunvidia <58281909+gunvidia@users.noreply.github.com>
2019-11-29 17:45:26 +0900

    Update cast_score.js

--- cast_score.js
+++ cast_score.js
@@ -9,7 +9,7 @@ var data2 = document.querySelectorAll('.block_playdata_02_text');
  
 // キャスト別戦跡詳細ページ以外での起動はエラー
 if(!/^https:\/\/wonderland-wars.net\/castdetail.html$/.test(location.href)) {
-     return alert("※キャスト別戦跡詳細ページで実行して下さい※");
+     return alert("※キャスト別戦跡詳細ページで実行して下さい※"),0;
 }
  
 // 勝利数・敗北数・勝率
